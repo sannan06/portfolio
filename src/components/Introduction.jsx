bb77193e770b33c4606e4721b5903f6bcae628c4
sannanhafeez <sannanhafeez06@gmail.com>
2019-08-28 10:39:57 +1200

    Created rough layout for introduction

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import headshot from "../images/headshot.jpg";
+
+export default function Introduction() {
+    return (
+        <React.Fragment>
+            <div className="intro">
+                <div className="row">
+                    <div className="col-5 offset-md-7">
+                        <h1>Sannan Hafeez</h1>
+                        <h2>Software Engineering Student</h2>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
