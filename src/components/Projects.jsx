dac19ea8420f4e8c2d9e2ffb084105cf875fe6e3
sannanhafeez <sannanhafeez06@gmail.com>
2020-02-25 12:13:04 +1300

    Add content for projects, and adjust layout slightly

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
 
+import planit from "../images/planit/planit-3.png";
+import recipe from "../images/recipe/recipe-1.png";
+
 export default function About() {
     return (
         <React.Fragment>
@@ -24,43 +27,37 @@ export default function About() {
                 </Fade>
 
                 <Fade>
-                    <div className="row justify-content-md-center">
-                        <div className="col-5">
+                    <div className="row justify-content-md-center projects">
+                        <div className="col-6">
                             <div className="row">
-                                <div className="col">
-                                    <h2>Image here</h2>
+                                <div className="col image">
+                                    <img src={planit} alt="Planit" />
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="col">
                                     <h2 className="subheading">Planit</h2>
                                     <p className="body-paragraph">
-                                        Lorem ipsum dolor sit amet, consectetur
-                                        adipiscing elit. Etiam et fermentum dui.
-                                        Ut orci quam, ornare sed lorem sed,
-                                        hendrerit auctor dolor. Nulla viverra,
-                                        nibh quis ultrices malesuada, ligula
-                                        ipsum vulputate diam, aliquam egestas
-                                        nibh ante vel dui. Sed in tellus
-                                        interdum eros vulputate placerat sed non
-                                        enim. Pellentesque eget. Lorem ipsum
-                                        dolor sit amet, consectetur adipiscing
-                                        elit. Etiam et fermentum dui. Ut orci
-                                        quam, ornare sed lorem sed, hendrerit
-                                        auctor dolor.
+                                        A proof-of-concept for a service
+                                        handling all aspects of travel planning,
+                                        from flights, hotels, attractions etc,
+                                        with smart recommendations based on
+                                        travellers. Created during DEVS
+                                        hackathon using React.JS. Project
+                                        involved market research and validation,
+                                        revenue analysis, project timeline, and
+                                        a presentation to a panel of judges
                                     </p>
                                 </div>
                             </div>
                         </div>
-                        {/* </div> */}
-                        {/* <div
-                        className="row justify-content-md-center"
-                        style={{ marginTop: "1.3rem" }}
-                    > */}
-                        <div className="col-5">
+                    </div>
+
+                    <div className="row justify-content-md-center projects">
+                        <div className="col-6">
                             <div className="row">
-                                <div className="col">
-                                    <h2>Image here</h2>
+                                <div className="col image">
+                                    <img src={recipe} alt="Recipe Search" />
                                 </div>
                             </div>
                             <div className="row">
@@ -69,19 +66,16 @@ export default function About() {
                                         Recipe Search
                                     </h2>
                                     <p className="body-paragraph">
-                                        Lorem ipsum dolor sit amet, consectetur
-                                        adipiscing elit. Etiam et fermentum dui.
-                                        Ut orci quam, ornare sed lorem sed,
-                                        hendrerit auctor dolor. Nulla viverra,
-                                        nibh quis ultrices malesuada, ligula
-                                        ipsum vulputate diam, aliquam egestas
-                                        nibh ante vel dui. Sed in tellus
-                                        interdum eros vulputate placerat sed non
-                                        enim. Pellentesque eget. Lorem ipsum
-                                        dolor sit amet, consectetur adipiscing
-                                        elit. Etiam et fermentum dui. Ut orci
-                                        quam, ornare sed lorem sed, hendrerit
-                                        auctor dolor.
+                                        An online recipe bank web-app to store
+                                        and collaborate on food recipes.
+                                        Multiple user system, with
+                                        authentication. Frontend with React
+                                        TypeScript, backend with C# and ASP.NET
+                                        Core, hosted as Azure SQL with blob
+                                        storage. Developed as submission for
+                                        Microsoft Student Accelerator 2018, and
+                                        presented as a potential product to
+                                        judges.
                                     </p>
                                 </div>
                             </div>
