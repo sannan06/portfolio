7316ef4e2aa9c0be9e89c4759e7a9d6c2c005195
sannanhafeez <sannanhafeez06@gmail.com>
2020-02-27 15:23:35 +1300

    Write express getter to trigger PDF download

--- functions/cv-download/cv-download.js
+++ functions/cv-download/cv-download.js
@@ -1,7 +1,10 @@
-exports.handler = async event => {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-        statusCode: 200,
-        body: `Hello ${subject}!`,
-    }
-}
+'use strict';
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+
+app.get('/download', (req, res) => {
+    res.download("./cv.pdf");
+})
+
+module.exports.handler = serverless(app);
