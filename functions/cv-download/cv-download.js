3a897a53681f9c064f25a3d2362a3a5bae52315e
sannanhafeez <sannanhafeez06@gmail.com>
2020-02-27 16:12:24 +1300

    Add content-disposition header to lambda function

--- functions/cv-download/cv-download.js
+++ functions/cv-download/cv-download.js
@@ -5,7 +5,7 @@ const file = fs.readFileSync("./cv.pdf")
 exports.handler = function (event, context, callback) {
     let response = {
         statusCode: 200,
-        headers: { 'Content-type': 'application/pdf' },
+        headers: { 'Content-type': 'application/pdf', 'Content-Disposition': 'attachment; filename=Sannan_Hafeez_CV.pdf' },
         body: file.toString('base64'),
         isBase64Encoded: true,
     };
