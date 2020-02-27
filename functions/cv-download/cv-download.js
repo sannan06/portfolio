2a0c845a0fea3ecea4e5e91534a7afaa763d3e4e
sannanhafeez <sannanhafeez06@gmail.com>
2020-02-27 16:00:45 +1300

    Encode PDF in base64

--- functions/cv-download/cv-download.js
+++ functions/cv-download/cv-download.js
@@ -3,8 +3,12 @@ const fs = require("fs")
 const file = fs.readFileSync("./cv.pdf")
 
 exports.handler = function (event, context, callback) {
-    callback(null, {
+    let response = {
         statusCode: 200,
-        body: `${file}`
-    })
+        headers: { 'Content-type': 'application/pdf' },
+        body: file.toString('base64'),
+        isBase64Encoded: true,
+    };
+
+    return callback(null, response);
 }
